ec9ec93ba531ec3145fdfdf2e9096f5e017a344a
hanjiang.Yue <yue571041524@qq.com>
2022-06-26 15:59:00 +0800

    更新数据

--- src/main/java/com/github/nkinsp/myspringjdbc/code/Rows.java
+++ src/main/java/com/github/nkinsp/myspringjdbc/code/Rows.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -72,6 +73,10 @@ public class Rows<E> extends ArrayList<E> {
 	
 	public Rows<E> distinct(){
 		return  stream(x->x.distinct());
+	}
+	
+	public Rows<E> filter(Predicate<E> predicate){
+		return  stream(x->x.filter(predicate));
 	}
 	
 	public <A,R>  R collect(Collector<E, A, R> collector) {
